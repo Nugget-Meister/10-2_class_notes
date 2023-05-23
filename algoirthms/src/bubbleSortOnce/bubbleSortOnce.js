7eb5c5a1bc4968029d7ba8c427a9edfe6e368ea6
Nugget-Meister <tremainecamacho@gmail.com>
2023-05-23 19:14:20 -0400

    bubble sort

--- algoirthms/src/bubbleSortOnce/bubbleSortOnce.js
+++ algoirthms/src/bubbleSortOnce/bubbleSortOnce.js
@@ -4,6 +4,7 @@
  * @param {number[]} arr - An array of integers to sort.
  * @returns {number[]} - A partialy sorted array where the largest element has 'bubbled' up to the end of the array.
  */
+<<<<<<< HEAD
 // we need to compare the first item to the next, and then swap them if they're out of order;
     // we then coninue to do this until we have reached the end of the array
     
@@ -23,6 +24,21 @@ const bubbleSortOnce = (arr) => {
     } 
     return arr
         // do nothing - keep going
-}
+=======
 
+const bubbleSortOnce = (a) => {
+let newArray = a;
+    for (let i = 0; i < a.length-1; i++){
+        let first = a[i]
+        let second = a[i+1]
+        if(first > second) {
+            a[i] = second;
+            a[i+1] = first;
+        }
+    }
+   return newArray
+>>>>>>> a90fa9c (bubblesort)
+}
+//[3,5,6,7,2,8]
+console.log(bubbleSortOnce([5, 3, 8, 6, 7, 2]))
 module.exports = { bubbleSortOnce };
