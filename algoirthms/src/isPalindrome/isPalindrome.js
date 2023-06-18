592505a736e6d019124f09b5d701dfa2d3188131
Nugget-Meister <tremainecamacho@gmail.com>
2023-06-18 08:55:41 -0400

    yeah

--- algoirthms/src/isPalindrome/isPalindrome.js
+++ algoirthms/src/isPalindrome/isPalindrome.js
@@ -5,10 +5,17 @@
  * @returns {boolean} - Returns true if the string is a palindrome, and false otherwise.
  */
 function isPalindrome(str) {
-    
+    str = str.toLowerCase()
+    for(i = 0; i < str.length; i++) {
+        if (str[i] != str[str.length-(i+1)]){
+            return false
+        }
+    }
+    return true
 }
 
 
+    // return str == str.split('').reverse().join('')
 module.exports = {
     isPalindrome
 }
